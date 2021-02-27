36e068623f8ca0e89f95af187c64f6472fc6e053
Rafael Gomes <faelfg@gmail.com>
2021-02-26 23:32:33 -0300

    Initial commit

---
+++ src/test/java/br/com/breakforwhile/bankline/BanklineApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.breakforwhile.bankline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanklineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
